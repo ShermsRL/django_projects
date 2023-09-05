797e68fc4627d4b362a3bc3d03c21381a17ddca7
Sherman <shermantaymk@gmail.com>
2023-09-05 22:39:28 +0800

    complete html and css

--- haiku_website/website/urls.py
+++ haiku_website/website/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='website-home'),
-    path('about/', views.about, name='website-about')
+    path('about/', views.about, name='website-about'),
+    path('haikus/', views.haikus, name='website-haikus'),
 ]
--- haiku_website/website/views.py
+++ haiku_website/website/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+haiku_data = [
+    {
+        'author': 'Sherman Tay',
+        'title': 'Haiku of the day',
+        'haiku1': 'abcde',
+        'haiku2': 'fghijkl',
+        'haiku3': 'mnopq',
+        'date_posted': 'September 4, 2023'
+    },
+    {
+        'author': 'Sherman Tay',
+        'title': 'Haiku of the day',
+        'haiku1': 'abcde',
+        'haiku2': 'fghijkl',
+        'haiku3': 'mnopq',
+        'date_posted': 'September 4, 2023'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Website Home </h1>')
+    context = {
+        'author': 'Sherman Tay',
+        'title': 'Haiku of the day',
+        'haiku1': 'abcde',
+        'haiku2': 'fghijkl',
+        'haiku3': 'mnopq',
+        'date_posted': 'September 4, 2023'
+    }
+
+    return render(request, 'website/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Website About </h1>')
+    return render(request, 'website/about.html', {'title': 'About'})
+
+
+def haikus(request):
+    context = {
+        'haikus': haiku_data
+    }
+
+    return render(request, 'website/haikus.html', context)
