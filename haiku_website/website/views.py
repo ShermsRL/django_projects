34e6d19f3b9594444cc8922870c59c3fec677017
Sherman <shermantaymk@gmail.com>
2023-09-06 22:30:06 +0800

    remove test data

--- haiku_website/website/views.py
+++ haiku_website/website/views.py
@@ -1,34 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-haiku_data = [
-    {
-        'author': 'Sherman Tay',
-        'title': 'Haiku of the day',
-        'haiku1': 'abcde',
-        'haiku2': 'fghijkl',
-        'haiku3': 'mnopq',
-        'date_posted': 'September 4, 2023'
-    },
-    {
-        'author': 'Sherman Tay',
-        'title': 'Haiku of the day',
-        'haiku1': 'abcde',
-        'haiku2': 'fghijkl',
-        'haiku3': 'mnopq',
-        'date_posted': 'September 4, 2023'
-    },
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'author': 'Sherman Tay',
-        'title': 'Haiku of the day',
-        'haiku1': 'abcde',
-        'haiku2': 'fghijkl',
-        'haiku3': 'mnopq',
-        'date_posted': 'September 4, 2023'
+        'haikus': Post.objects.get(id=1)
     }
 
     return render(request, 'website/home.html', context)
@@ -40,7 +17,7 @@ def about(request):
 
 def haikus(request):
     context = {
-        'haikus': haiku_data
+        'haikus': Post.objects.all()
     }
 
     return render(request, 'website/haikus.html', context)
